bd4a0c0d061df9cd4fc9ba2a25b1295a68e2aff3
Victoria Fajardo <victoria.fajardo@gmail.com>
2016-03-18 12:33:17 -0500

    server

---
+++ app.rb
@@ -0,0 +1,16 @@
+require 'bundler'
+Bundler.require
+
+get '/' do
+  {
+    :message => 'Hi world, I am on the interwebs.',
+    :status => '200',
+    :data => 'Some data'
+  }.to_json
+
+end
+
+get '/happy' do
+  erb :happy
+
+end
